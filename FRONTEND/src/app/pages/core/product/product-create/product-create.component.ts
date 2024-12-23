a7db0fa47215ab98d1f70aa20accf0cb3ac2c492
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-12-23 17:43:59 +0100

    continue working

--- FRONTEND/src/app/pages/core/product/product-create/product-create.component.ts
+++ FRONTEND/src/app/pages/core/product/product-create/product-create.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatButton,
+    MatFormField,
+    MatHint,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule
+  ],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.scss'
 })
@@ -11,5 +23,23 @@ export class ProductCreateComponent {
  constructor() {
  }
 
+  form = new FormGroup({
+    token: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+
+    // this.authService.verify(this.form.controls['token'].value).subscribe(res => {
+    //     console.log(res);
+    //   }
+    // )
+    // this.router.navigate(['/authentication/login']);
+  }
 
 }
